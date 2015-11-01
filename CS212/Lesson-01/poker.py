dd0d14bffcc271325aec0e2f282e673bab42d0f7
Zachary Jones <zacharytamas@gmail.com>
2015-11-01 14:41:56 -0500

    Implement two_pair()
    I believe mine is more efficient than his, actually. Mine is only O(N).

--- CS212/Lesson-01/poker.py
+++ CS212/Lesson-01/poker.py
@@ -75,11 +75,26 @@ def kind(n, ranks):
     return last
 
 
+def two_pair(ranks):
+  """If there are two pair, return the two ranks as a
+  tuple: (highest, lowest); otherwise return None."""
+
+  pairs = set()
+
+  for i in range(len(ranks) - 1):
+    if ranks[i] == ranks[i + 1]:
+      pairs.add(ranks[i])
+
+  if len(pairs) == 2:
+    return tuple(pairs)
+
+
 def test():
     "Test cases for the functions in poker program"
     sf = "6C 7C 8C 9C TC".split()  # Straight Flush
     fk = "9D 9H 9S 9C 7D".split()  # Four of a Kind
     fh = "TD TC TH 7C 7D".split()  # Full House
+    tp = "5S 5D 9H 9C 6S".split()
 
     assert card_ranks(sf) == [10, 9, 8, 7, 6]
     assert card_ranks(fk) == [9, 9, 9, 9, 7]
@@ -95,6 +110,9 @@ def test():
     assert hand_rank(sf) == (8, 10)
     assert hand_rank(fk) == (7, 9, 7)
     assert hand_rank(fh) == (6, 10, 7)
+
+    assert two_pair(fkranks) is None
+    assert two_pair(card_ranks(tp)) == (9, 5)
 
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
