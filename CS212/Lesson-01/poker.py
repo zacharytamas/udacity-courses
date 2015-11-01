ed71e00f20eb660edf664f67fd056cfdee4899dd
Zachary Jones <zacharytamas@gmail.com>
2015-10-31 22:17:19 -0400

    Okay I am going to deviate from his style a bit.

--- CS212/Lesson-01/poker.py
+++ CS212/Lesson-01/poker.py
@@ -4,24 +4,30 @@
 #
 
 def poker(hands):
-    "Return the best hand: poker([hand,...]) => hand"
-    return max(hands, key=hand_rank)
+  "Return the best hand: poker([hand,...]) => hand"
+  return max(hands, key=hand_rank)
 
 
 def hand_rank(hand):
+  "Return a value indicating the rank of a hand."
+  ranks = card_ranks(hand)
   return hand
 
 
+def card_ranks(hand):
+  pass
+
+
 def test():
-    "Test cases for the functions in poker program"
-    sf = "6C 7C 8C 9C TC".split()
-    fk = "9D 9H 9S 9C 7D".split()
-    fh = "TD TC TH 7C 7D".split()
-    assert poker([sf, fk, fh]) == sf
-    assert poker([fk, fh]) == fk
-    assert poker([fh, fh]) == fh
-    assert poker([fh]) == fh
-    assert poker([sf] + 99 * [fh]) == sf
-    return 'tests pass'
+  "Test cases for the functions in poker program"
+  sf = "6C 7C 8C 9C TC".split()
+  fk = "9D 9H 9S 9C 7D".split()
+  fh = "TD TC TH 7C 7D".split()
+  assert poker([sf, fk, fh]) == sf
+  assert poker([fk, fh]) == fk
+  assert poker([fh, fh]) == fh
+  assert poker([fh]) == fh
+  assert poker([sf] + 99 * [fh]) == sf
+  return 'tests pass'
 
 print test()
