e01c2b402ca71ff8a47e9d4673c6698896a8bd36
Zachary Jones <zacharytamas@gmail.com>
2015-10-31 22:11:42 -0400

    More tests.

--- CS212/Lesson-01/poker.py
+++ CS212/Lesson-01/poker.py
@@ -18,12 +18,10 @@ def test():
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
     assert poker([sf, fk, fh]) == sf
-
-    # Add 2 new assert statements here. The first
-    # should check that when fk plays fh, fk
-    # is the winner. The second should confirm that
-    # fh playing against fh returns fh.
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
+    assert poker([fh]) == fh
+    assert poker([fh for i in range(100)]) == fh
+    return 'tests pass'
 
 print test()
