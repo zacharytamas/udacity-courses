93826ea2b5c6e2395f7603d2ab88c737102ef9ba
Zachary Jones <zacharytamas@gmail.com>
2015-10-31 22:41:28 -0400

    My hand_rank version.

--- CS212/Lesson-01/poker.py
+++ CS212/Lesson-01/poker.py
@@ -14,19 +14,19 @@ def hand_rank(hand):
     elif kind(4, ranks):                           # 4 of a kind
         return (7, kind(4, ranks), kind(1, ranks))
     elif kind(3, ranks) and kind(2, ranks):        # full house
-        return # your code here
+        return (6, kind(3, ranks), kind(2, ranks))
     elif flush(hand):                              # flush
-        return # your code here
+        return (5, ranks)
     elif straight(ranks):                          # straight
-        return # your code here
+        return (4, max(hand))
     elif kind(3, ranks):                           # 3 of a kind
-        return # your code here
+        return (3, kind(3, ranks), ranks)
     elif two_pair(ranks):                          # 2 pair
-        return # your code here
+        return (2, two_pair(ranks))
     elif kind(2, ranks):                           # kind
-        return # your code here
+        return (1, ranks)
     else:                                          # high card
-        return # your code here
+        return (0, ranks)
 
 def test():
     "Test cases for the functions in poker program"
