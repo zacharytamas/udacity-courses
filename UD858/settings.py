fcab444b8da6f4315b04b41738cf07c5f3473f69
Zachary Jones <zacharytamas@gmail.com>
2015-11-09 17:00:01 -0500

    Add oauth2 client id

--- UD858/settings.py
+++ UD858/settings.py
@@ -12,5 +12,4 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
-
+WEB_CLIENT_ID = '897025632095-df4358n84k27aaf0rj0dff52db4dtgh8.apps.googleusercontent.com'
--- UD858/static/js/app.js
+++ UD858/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '897025632095-df4358n84k27aaf0rj0dff52db4dtgh8.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
